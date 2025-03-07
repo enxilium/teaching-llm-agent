9decfef7df3c9c8426b4708df69504478a7ac079
enxilium <jace.zk.dev@gmail.com>
2025-03-06 22:57:25 -0500

    fixing css errors

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: false,
-  basePath: process.env.NODE_ENV === "production" ? "/teaching-llm-agent" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/teaching-llm-agent/" : "",
   images: {
     unoptimized: true,
   },
